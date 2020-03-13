bc93eee00736f84ccd4bc9169d76714cea9ae114
ux33-331 <ux33-331@protonmail.com>
2020-03-14 00:35:09 +0100

    minimal

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 const config = {
-    pool: "pool.kryptokrona.se",	// pool url
-    port: 11898,	// pool port
-    wallet: "SEKReX2avthCKT4YUUKV3jgZ1Hderk9XbRciqp8vHVPoDSb9nA1dCV86Jia3TkD4jWgfxeh1AEYV3DKEAesSb7mSAvNqf6cB6kR", // your wallet address
-    speed: 100,		// cpu speed/usage in %
-    threads: 4,		// number of threads using for mining
-    workerName: "WebMiner"	// the miner name also knows as 'password' for the pool, default 'x'
+    pool: "pool.kryptokrona.se", // pool url
+    port: 3333, // pool port
+    wallet: "", // your wallet address
+    speed: 100, // cpu speed/usage in %
+    threads: 4, // number of threads using for mining
+    workerName: "x" // the miner name also knows as 'password' for the pool, default 'x'
 }
 const miner = new kryptokronaMiner(config);
 console.log(miner.dump());
